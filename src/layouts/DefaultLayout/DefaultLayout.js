b4d9f4477d722a5705816f725f9fb808c2ce6a05
Withyou11 <100573436+Withyou11@users.noreply.github.com>
2023-11-01 10:26:56 +0700

    complete homepage

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,15 +1,19 @@
 import classNames from 'classnames/bind';
 import styles from './DefaultLayout.module.scss';
+import CustomerHeader from '~/layouts/components/CustomerHeader/CustomerHeader';
+import Footer from '../Footer/Footer';
 const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
         <div>
+            <CustomerHeader />
             <div className={cx('wrapper')}>
                 <div className={cx('container')}>
                     <div className={cx('content')}>{children}</div>
                 </div>
             </div>
+            <Footer />
         </div>
     );
 }
